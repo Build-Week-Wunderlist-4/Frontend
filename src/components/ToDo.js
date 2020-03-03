abacebc8c52603767669ae8732676fe803c446e3
alexis villaraza <alexis.borja@gmail.com>
2020-03-03 16:46:06 -0700

    merged from master

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
 
-const ToDo = (props) => {
-    const [input, setInput] = useState({
-        Task: ''
-    });
+const ToDo = props => {
+  const [input, setInput] = useState({
+    Task: ""
+  });
 
-    const handleChanges = e => {
-        setInput({ ...input, [e.target.name]: e.target.value });
-    }
+  const handleChanges = e => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
 
-    const submitTask = e => {
-        e.preventDefault();
-        // 
-    }
+  const submitTask = e => {
+    e.preventDefault();
+    //
+  };
 
-    return (
-        <form onSubmit={submitTask}>
-            <label htmlFor="Task">Task</label>
-            <input id="Task" name="Task" type="text" placeholder="Get this done" value={input.name} onChange={handleChanges} />
+  return (
+    <form onSubmit={submitTask}>
+      <label htmlFor="Task">Task</label>
+      <input
+        id="Task"
+        name="Task"
+        type="text"
+        placeholder="Get this done"
+        value={input.name}
+        onChange={handleChanges}
+      />
 
-            <button type="submit">Submit</button>
-        </form>
-    )
-
-}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default ToDo;
